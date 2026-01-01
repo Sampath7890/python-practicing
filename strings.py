d889dc25d317eaaede6f6f664b67a1cb3b3398db
Sampath7890 <gsksd64@gmail.com>
2026-01-01 17:33:27 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,17 @@
+str1="Sampath Kumar"
+print(str1[1:3])
+
+str1="Sampath Kumar"
+print(str1[8:len(str1)])
+
+print(len(str1))
+
+print(str1.endswith("r"))
+
+print(str1.count("a"))
+
+print(str1.capitalize())
+
+print(str1.find("Kumar"))
+
+print(str1.lower())
