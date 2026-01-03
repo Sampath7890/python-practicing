d76ac1365e1c721db8599d75a1728394128b225c
Sampath7890 <gsksd64@gmail.com>
2026-01-03 22:14:59 +0530

    practiced python

---
+++ pythonifelse.py
@@ -0,0 +1,23 @@
+"""Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Input Format
+
+A single line containing a positive integer, ."""
+
+
+
+n = int(input())
+if n%2!=0:
+    print("Weird")
+else:
+    if 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")    
+    else:
+        print("Not Weird")      
---
+++ sets2.py
@@ -0,0 +1,6 @@
+set1 = {10,20,30,40}
+set2 ={20,50,40,80}
+print(set1.union(set2))
+print(set1)
+print(set2)
+print(set1.intersection(set2))
